3fc633cfd0646a7172be9690ef0c3e0a6da5350f
Cai Mingda <caimingda@Cais-Mac-mini.local>
2024-12-29 22:15:41 +0800

    Unit test for GetBlockByNumber

--- internal/ethgateway/test/get_block_by_number_test.go
+++ internal/ethgateway/test/get_block_by_number_test.go
@@ -26,6 +26,36 @@ func Test_impl_GetBlockByNumber(t *testing.T) {
 			args: args{
 				number: "0x1476e8c",
 			},
+			want: model.Block{
+				Number: 21458572,
+				Transactions: []model.Transaction{
+					{
+						From:   "0x042523db4f3effc33d2742022b2490258494f8b3",
+						To:     "0xa69babef1ca67a37ffaf7a485dfff3382056e78c",
+						Amount: "0xfcac00",
+					},
+					{
+						From:   "0x91aae0aafd9d2d730111b395c6871f248d7bd728",
+						To:     "0x98c3d3183c4b8a650614ad179a1a98be0a8d6b8e",
+						Amount: "0x0",
+					},
+					{
+						From:   "0x5e2b6c6b2240d582995537d3fafdb556e4a3822f",
+						To:     "0x98c3d3183c4b8a650614ad179a1a98be0a8d6b8e",
+						Amount: "0x0",
+					},
+					{
+						From:   "0x3a67b184803641ba107c4c75276d747d123e49ca",
+						To:     "0x51c72848c68a965f66fa7a88855f9f7784502a7f",
+						Amount: "0x0",
+					},
+					{
+						From:   "0x187bc65342c5c902e8c2084017c7aaf4ec53addf",
+						To:     "0x80a64c6d7f12c47b7c66c5b4e20e72bc1fcd5d9e",
+						Amount: "0xb1a2bc2ec50000",
+					},
+				},
+			},
 		},
 	}
 
@@ -39,6 +69,9 @@ func Test_impl_GetBlockByNumber(t *testing.T) {
 				t.Errorf("impl.GetBlockByNumber() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
+
+			// To many transactions in one block, just check the first 5 trasnactions.
+			got.Transactions = got.Transactions[:5]
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("impl.GetBlockByNumber() = %v, want %v", got, tt.want)
 			}
