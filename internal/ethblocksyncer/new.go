2618d1079f3c396befb1c7b536560203bb682be6
Cai Mingda <caimingda@Cais-Mac-mini.local>
2025-01-04 23:57:52 +0800

    Implement ethereum block syncer

--- internal/ethblocksyncer/new.go
+++ internal/ethblocksyncer/new.go
@@ -1,1 +1,18 @@
 package ethblocksyncer
+
+import (
+	"github.com/Unbeliever1987/ethparser/internal/ethgateway"
+	"github.com/Unbeliever1987/ethparser/internal/repository"
+)
+
+type Runner struct {
+	repository repository.Repository
+	ethGateway ethgateway.Gateway
+}
+
+func New(repository repository.Repository, ethGateway ethgateway.Gateway) Runner {
+	return Runner{
+		repository: repository,
+		ethGateway: ethGateway,
+	}
+}
